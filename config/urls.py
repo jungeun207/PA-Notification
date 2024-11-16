f059fee741018ff25537b9ce3b547074cdd018d9
정은 <149307733+jungeun207@users.noreply.github.com>
2024-11-16 23:23:40 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('schedule.urls')),
+    path('', views.grade_selection, include('schedule.urls')),
 ]
